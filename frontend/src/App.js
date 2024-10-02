fc2f453d73c1d3b64641ce4bb18c55fb31578039
Simon Truong <109702333+Trilinear@users.noreply.github.com>
2024-10-02 09:45:37 -0700

    Added login functionality, react routers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,99 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
 import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-
-
-// function ProcessLogin() {
-
-// }
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Landing } from "./pages/Landing";
+import { Login } from "./pages/Login";
 
 function App() {
-  const [inputs, setInputs] = useState({});
-
-  const handleSetValue = (event) => {
-    const entry = event.target.name;
-    const pass = event.target.value;
-    setInputs(values => ({...values, [entry]: pass}))
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (inputs.user == 'John' && inputs.pass == '123') {
-      alert("passed login check");
-    }
-  }
-
 
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+  
+    <div className="wrapper">
+      <Router>
+        <Routes>
+          <Route exact path = "/" element={<Landing />}/>
+          <Route path = "/login" element={<Login />}/>
+        </Routes>
+      </Router>
+    </div>
 
-    <body>
-      <div className="nav">
-        <div className="left-section">
-          <div className="logo">Let It Fly</div>
-            <ul>
-              <li><a href="#">Ride</a></li>
-              <li><a href="#">Drive</a></li>
-              <li><a href="#">About</a></li>
-            </ul>
-        </div>
-        <ul>
-          <li><a href="LoginPage.js">Log in</a></li>
-          <li><a href="#">Sign up</a></li>
-        </ul>
-      </div>
-
-
-      <div className="numerical">
-        <div className="container">
-          
-        <form onSubmit={handleSubmit}>
-          <label>Enter your username:
-            <br/>
-            <input 
-              type="text" 
-              name="user"
-              value={inputs.user || ""}
-              onChange={handleSetValue}
-            />
-          </label>
-          <br/>
-          <label>Enter your pass:
-            <br/>
-            <input 
-              type="text" 
-              name="pass"
-              value={inputs.pass || ""}
-              onChange={handleSetValue}
-            />
-          </label>
-          <br/>
-          <input type="submit" />
-        </form>
-
-        </div>
-      </div>
-
-      
-    </body>
   );
 }
 
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import './App.css';
+
+
+
+export const Login = () => {
+
+    const [inputs, setInputs] = useState({});
+    const handleSetValue = (event) => {
+      const entry = event.target.name;
+      const pass = event.target.value;
+      setInputs(values => ({...values, [entry]: pass}))
+    }
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      // Sample test case for the sprint this week
+      if (inputs.email == "john123@gmail.com" && inputs.pass == "123") {
+        //
+        alert("The login is successful! Redirecting back to landing page for this demo");
+        window.location.replace("/");
+      }
+    }
+
+
+    return (
+
+        <body>
+            <div className="custom-nav">
+                <div className="left-section">
+                    <div className="logo">Let It Fly</div>
+                    <ul>
+                        <li><a href="#">Ride</a></li>
+                        <li><a href="#">Drive</a></li>
+                        <li><a href="#">About</a></li>
+                    </ul>
+                </div>
+                <ul>
+                    <li><Link to="/login" className='nav-button'>Log in</Link></li>
+                    <li><a href="#">Sign up</a></li>
+                </ul>
+            </div>
+                
+            <div className="numerical">
+                <div className="container">
+                    <form className="form-group custom-form" onSubmit={handleSubmit}>
+                        <label>Enter your email:</label>
+                    <br/>
+                        <input 
+                            type="email" 
+                            name="email"
+                            required className = 'form-control'
+                            value={inputs.email || ""}
+                            onChange={handleSetValue}
+                        />
+
+                        <br/>
+                        <label>Enter your pass:</label>
+                        <br/>
+                        <input 
+                            type="password" 
+                            name="pass"
+                            required className = 'form-control'
+                            value={inputs.pass || ""}
+                            onChange={handleSetValue}
+                        />
+                    <br/>
+
+
+                    <br/>
+                        <button type='submit' className='btn btn-success btn-md'>Login</button>
+                    </form>
+                </div>
+            </div>
+        </body>
+    )
+}
