9bcc0e78fa19974d503d1497ddfea399c1f6ebd7
Simon Truong <109702333+Trilinear@users.noreply.github.com>
2024-10-16 11:13:54 -0700

    Routing added for driver

--- frontend/src/pages/Driver.js
+++ frontend/src/pages/Driver.js
@@ -6,57 +6,88 @@ import { useRef, useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 import { Geocoder } from '@mapbox/search-js-react';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import MapBoxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions'
+
 
 export const Driver = () => {
     const mapRef = useRef();
     const mapContainerRef = useRef();
-    const marker = useRef();
+    const markerRef = useRef();
+    const routingRef = useRef();
     const [, setMapLoaded] = useState(false);
     const [inputValue, setInputValue] = useState("");
+    const sjcLatLong = [-121.92857174599622, 37.36353799938156];
     var currentLat = 0;
     var currentLong = 0;
 
+    const getDirection = () => {
+        // Note: right now calling this more than once causes the route to change because the center
+        // point on the map changes every time
+        routingRef.current.setOrigin([mapRef.current.getCenter().lng, mapRef.current.getCenter().lat]);
+        routingRef.current.setDestination(sjcLatLong);
+    }
+
+
     const currentPos = () => {
+        // Checks if the browser and device has geolocation enabled 
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(
-                storePos
+                updatePos
             );
-    
         } else {
             console.log("This browser does not support geolocation");
         }
     };
 
-    const storePos = (position) => {
+    const updatePos = (position) => {
         currentLat = position.coords.latitude;
         currentLong = position.coords.longitude;
-        console.log("Current pos is " + currentLat + " and " + currentLong);
-        mapRef.current.setCenter([currentLong, currentLat]);
-        marker.current.setLngLat([mapRef.current.getCenter().lng, mapRef.current.getCenter().lat]);
-    }
 
+        // Debug Line: 
+        // console.log("Current pos is " + currentLat + " and " + currentLong);
+
+        // Update map and marker to be on center to current location
+        mapRef.current.setCenter([currentLong, currentLat]);
+        markerRef.current.setLngLat([mapRef.current.getCenter().lng, mapRef.current.getCenter().lat]);
+    }
 
 
     useEffect(() => {
         mapboxgl.accessToken = process.env.REACT_APP_SECRET_MAP_KEY;
         mapRef.current = new mapboxgl.Map({
             container: mapContainerRef.current, // Container
-            center: [-121.92857174599622, 37.36353799938156], // Starting pos
+            center: [-121.92857174599622, 37.36353799938156], // Starting pos from SJC
             zoom: 14 // Starting zoom
         });
 
-        marker.current = new mapboxgl.Marker()
+        markerRef.current = new mapboxgl.Marker()
             .setLngLat([mapRef.current.getCenter().lng, mapRef.current.getCenter().lat])
             .addTo(mapRef.current);
-        
-        mapRef.current.on("load", () => {
-            setMapLoaded(true);
+
+            
+        routingRef.current = new MapBoxDirections({
+            accessToken: mapboxgl.accessToken,
+            unit:'imperial',
+            profile:'mapbox/driving-traffic',
+            interactive: false,
+            controls: {
+                instructions: false,
+    
+            }
         });
 
+        mapRef.current.on("load", () => {
+            setMapLoaded(true);
+            console.log("map loaded");
+        });
+            
+        mapRef.current.addControl(routingRef.current);
+        
         return () => {
             mapRef.current.remove();
         }
     }, []);
+
 
 
 
@@ -78,17 +109,19 @@ export const Driver = () => {
             </div>
 
             <div className='box-container'>
-
-
-                
-
                 <div className='left-column text-center'>
                 <br/><br/><br/><br/><br/><br/><br/>
                     <img src = '/default-profile.png'></img><br/>
                     {/* This is a placeholder, replace with JS elements that get name from DB */}
                     <span id='name-text'>John Doe</span><br/><br/>
                     <button className='btn btn-primary btn-circle btn-lg' onClick={currentPos}>Start Work</button>
+
+
                     <div className='disclaimer-text'>Note: This will use your location</div><br/>
+
+
+                    <button className='btn btn-primary btn-circle btn-lg' onClick={getDirection}>Get path from current map center to SJC</button>
+
                     {/* This is a placeholder, replace with JS elements that get license from DB */}
                     <h6>License Plate: 0tst000</h6>
                 </div>      
@@ -102,6 +135,7 @@ export const Driver = () => {
                             setInputValue(e);
                         }}
                     />
+
                     <div className='map-container' ref={mapContainerRef}>
                     </div>
                 </div>   
