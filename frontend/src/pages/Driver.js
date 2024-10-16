7d6eb1a698e06e4007395aaf15470f97ed70d382
Simon Truong <109702333+Trilinear@users.noreply.github.com>
2024-10-16 00:38:33 -0700

    Implement MapBox map, geocoding, current location

--- frontend/src/pages/Driver.js
+++ frontend/src/pages/Driver.js
@@ -2,25 +2,78 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Driver.css';
 
+import { useRef, useEffect, useState } from 'react';
+import mapboxgl from 'mapbox-gl';
+import { Geocoder } from '@mapbox/search-js-react';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 export const Driver = () => {
-    const name = 'John Doe';
-    const license = "0tst000"
+    const mapRef = useRef();
+    const mapContainerRef = useRef();
+    const marker = useRef();
+    const [, setMapLoaded] = useState(false);
+    const [inputValue, setInputValue] = useState("");
+    var currentLat = 0;
+    var currentLong = 0;
 
+    const currentPos = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                storePos
+            );
     
+        } else {
+            console.log("This browser does not support geolocation");
+        }
+    };
+
+    const storePos = (position) => {
+        currentLat = position.coords.latitude;
+        currentLong = position.coords.longitude;
+        console.log("Current pos is " + currentLat + " and " + currentLong);
+        mapRef.current.setCenter([currentLong, currentLat]);
+        marker.current.setLngLat([mapRef.current.getCenter().lng, mapRef.current.getCenter().lat]);
+    }
+
+
+
+    useEffect(() => {
+        mapboxgl.accessToken = process.env.REACT_APP_SECRET_MAP_KEY;
+        mapRef.current = new mapboxgl.Map({
+            container: mapContainerRef.current, // Container
+            center: [-121.92857174599622, 37.36353799938156], // Starting pos
+            zoom: 14 // Starting zoom
+        });
+
+        marker.current = new mapboxgl.Marker()
+            .setLngLat([mapRef.current.getCenter().lng, mapRef.current.getCenter().lat])
+            .addTo(mapRef.current);
+        
+        mapRef.current.on("load", () => {
+            setMapLoaded(true);
+        });
+
+        return () => {
+            mapRef.current.remove();
+        }
+    }, []);
+
+
+
     return (
         <body>
             <div className="custom-nav">
                 <div className="left-section">
                     <div className="logo">Let It Fly</div>
-                    <ul>
+                        <ul>
                         <li><a href="#">Ride</a></li>
                         <li><a href="#">Drive</a></li>
                         <li><a href="#">About</a></li>
-                    </ul>
+                        </ul>
                 </div>
                 <ul>
-                    <li><a href="#">Account</a></li>
+                    <li><Link to="/login" className='nav-button'>Log in</Link></li>
+                    <li><Link to="/signup" className='nav-button'>Sign up</Link></li>
                 </ul>
             </div>
 
@@ -34,14 +87,22 @@ export const Driver = () => {
                     <img src = '/default-profile.png'></img><br/>
                     {/* This is a placeholder, replace with JS elements that get name from DB */}
                     <span id='name-text'>John Doe</span><br/><br/>
-                    <button className='btn btn-primary btn-circle btn-lg'>Start Work</button>
+                    <button className='btn btn-primary btn-circle btn-lg' onClick={currentPos}>Start Work</button>
                     <div className='disclaimer-text'>Note: This will use your location</div><br/>
                     {/* This is a placeholder, replace with JS elements that get license from DB */}
                     <h6>License Plate: 0tst000</h6>
                 </div>      
                 <div className='right-column'>
-                    <div className='image-container'>
-                        <img src='/SJC-map.png'></img>
+                    <Geocoder
+                        accessToken={mapboxgl.accessToken}
+                        map={mapRef.current}
+                        mapboxgl={mapboxgl}
+                        value={inputValue}
+                        onChange={(e) => {
+                            setInputValue(e);
+                        }}
+                    />
+                    <div className='map-container' ref={mapContainerRef}>
                     </div>
                 </div>   
 
